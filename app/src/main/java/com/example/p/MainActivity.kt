1d044d9d53ddc6437c1ea6b8e2387ee99506e0a5
Pablo <pabloalonsogarc@gmailc.om>
2022-10-30 00:16:36 +0200

    Ejercicio terminado

--- app/src/main/java/com/example/p/MainActivity.kt
+++ app/src/main/java/com/example/p/MainActivity.kt
@@ -1,5 +1,4 @@
 package com.example.p
-
 import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
@@ -7,38 +6,19 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
-
 class MainActivity : AppCompatActivity() {
-
     private lateinit var txtNombre: EditText
     private lateinit var btnAceptar: Button
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
         //Asignamos una referencia a los botones
         txtNombre = findViewById(R.id.txtNombre)
         btnAceptar = findViewById(R.id.btnAceptar)
-
-
-        /*btnAceptar.setOnClickListener {
-            //Creamos el Intent
-            val intent = Intent(this@MainActivity, SaludoActivity::class.java)
-            //Añadimos al intent la información a pasar entre actividades
-            intent.putExtra("NOMBRE", txtNombre.text.toString())
-            //Iniciamos la nueva actividad
-            startActivity(intent)
-        }*/
     }
-
     @SuppressLint("SuspiciousIndentation")
     fun aceptar(view: View) {
-
-
         val intent = Intent(this@MainActivity, SaludoActivity::class.java)
         //Añadimos al intent la información a pasar entre actividades
 
@@ -57,9 +37,4 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-
-
-
-
-
 }
